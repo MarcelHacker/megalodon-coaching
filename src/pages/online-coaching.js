bbca6ddc11e05abf6c707d0496e96744362c9b87
MarcelHacker <marcelhacker@dev.com>
2022-09-14 20:34:38 +0200

    Add images

--- src/pages/online-coaching.js
+++ src/pages/online-coaching.js
@@ -1,7 +1,37 @@
 import React from "react";
+import FigureComp from "../components/FigureComp";
+import plan from "../assets/plan.jpg";
+import coaching from "../assets/coaching.jpg";
 
-export default () => (
-  <div>
-    <h1>online Coaching</h1>
-  </div>
-);
+export default () => {
+  const width = 100;
+  const height = 100;
+  const images = [
+    {
+      alt: "Plan",
+      src: plan,
+      text: "Trainingsplan",
+    },
+    {
+      alt: "Plan",
+      src: coaching,
+      text: "Coaching",
+    },
+  ];
+  return (
+    <div>
+      <h1>Online Coaching</h1>
+      {images.map((image) => {
+        <div>
+          <FigureComp
+            width={width}
+            height={height}
+            alt={image.alt}
+            src={image.src}
+            text={image.text}
+          />
+        </div>;
+      })}
+    </div>
+  );
+};
