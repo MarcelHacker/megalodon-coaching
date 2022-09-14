72e0d80f330ecd86ae6016de749cb934a2350633
MarcelHacker <marcelhacker@dev.com>
2022-09-14 20:47:03 +0200

    Adjust online coaching site

--- src/pages/online-coaching.js
+++ src/pages/online-coaching.js
@@ -2,10 +2,11 @@ import React from "react";
 import FigureComp from "../components/FigureComp";
 import plan from "../assets/plan.jpg";
 import coaching from "../assets/coaching.jpg";
+import platform from "../assets/platform.jpg";
 
 export default () => {
-  const width = 100;
-  const height = 100;
+  const width = 320;
+  const height = 150;
   const images = [
     {
       alt: "Plan",
@@ -21,17 +22,37 @@ export default () => {
   return (
     <div>
       <h1>Online Coaching</h1>
-      {images.map((image) => {
-        <div>
-          <FigureComp
-            width={width}
-            height={height}
-            alt={image.alt}
-            src={image.src}
-            text={image.text}
-          />
-        </div>;
-      })}
+      <div>
+        <row>
+          <colum className="mx-2">
+            <FigureComp
+              width={width}
+              height={height}
+              alt={"Hallo"}
+              src={plan}
+              text={"Trainingsplan"}
+            />
+          </colum>
+          <colum className="mx-2">
+            <FigureComp
+              width={455}
+              height={height}
+              alt={"Hallo"}
+              src={coaching}
+              text={"Online Coaching"}
+            />
+          </colum>
+          <colum className="mx-2">
+            <FigureComp
+              width={width}
+              height={height}
+              alt={"Hallo"}
+              src={platform}
+              text={"Plattformcoaching"}
+            />
+          </colum>
+        </row>
+      </div>
     </div>
   );
 };
