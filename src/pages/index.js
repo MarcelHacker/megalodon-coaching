37b1d7f612d318dd8401323fbaf21c195018db87
Marcel Hacker <marcelhacker@Marcels-MacBook-Air.local>
2022-09-29 18:01:46 +0200

    Add test animation in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,30 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ShowcaseComp from "../components/ShowcaseComp";
 
+// tests
+import {useSpring} from "react-spring/web";
+
 import plan from "../assets/plan.jpg";
 
 export default () => {
   const width = "300";
   const height = "400";
+  const LoopObject=()=>{
+    const styles = useSpring({
+      loop: {reverse: true},
+      from: {x:0},
+      to:{x:100}
+    })
+  }
   return (
     <div style={{textAlign: "center"}}>
+      <animated.div>
+        style={{
+          width: 80
+        }}
+      </animated.div>
       <h1>Website in Arbeit...</h1>
+
       <Container>
         <Row>
           <Col>
