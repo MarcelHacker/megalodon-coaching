41740f63f21254192c5ec3291d3dd969b40536ec
MarcelHacker <marcelhacker@dev.com>
2022-10-01 15:47:30 +0200

    Add new logo

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -1,59 +1,18 @@
 
 
-import React from 'react'
-import universal, { setHasBabelPlugin } from 'react-universal-component'
-
-setHasBabelPlugin()
-
-const universalOptions = {
-  loading: () => null,
-  error: props => {
-    console.error(props.error);
-    return <div>An error occurred loading this page's template. More information is available in the console.</div>;
-  },
-  ignoreBabelRename: true
-}
-
-const t_0 = universal(import('__react_static_root__/src/pages/404.js'), universalOptions)
-      t_0.template = '__react_static_root__/src/pages/404.js'
-      
-const t_1 = universal(import('__react_static_root__/src/pages/about.js'), universalOptions)
-      t_1.template = '__react_static_root__/src/pages/about.js'
-      
-const t_2 = universal(import('__react_static_root__/src/pages/blog.js'), universalOptions)
-      t_2.template = '__react_static_root__/src/pages/blog.js'
-      
-const t_3 = universal(import('__react_static_root__/src/pages/contact.js'), universalOptions)
-      t_3.template = '__react_static_root__/src/pages/contact.js'
-      
-const t_4 = universal(import('__react_static_root__/src/pages/faq.js'), universalOptions)
-      t_4.template = '__react_static_root__/src/pages/faq.js'
-      
-const t_5 = universal(import('__react_static_root__/src/pages/index.js'), universalOptions)
-      t_5.template = '__react_static_root__/src/pages/index.js'
-      
-const t_6 = universal(import('__react_static_root__/src/pages/online-coaching.js'), universalOptions)
-      t_6.template = '__react_static_root__/src/pages/online-coaching.js'
-      
-const t_7 = universal(import('__react_static_root__/src/pages/qualifications.js'), universalOptions)
-      t_7.template = '__react_static_root__/src/pages/qualifications.js'
-      
-const t_8 = universal(import('__react_static_root__/src/containers/Post'), universalOptions)
-      t_8.template = '__react_static_root__/src/containers/Post'
-      
 
 // Template Map
 export default {
-  '__react_static_root__/src/pages/404.js': t_0,
-'__react_static_root__/src/pages/about.js': t_1,
-'__react_static_root__/src/pages/blog.js': t_2,
-'__react_static_root__/src/pages/contact.js': t_3,
-'__react_static_root__/src/pages/faq.js': t_4,
-'__react_static_root__/src/pages/index.js': t_5,
-'__react_static_root__/src/pages/online-coaching.js': t_6,
-'__react_static_root__/src/pages/qualifications.js': t_7,
-'__react_static_root__/src/containers/Post': t_8
+  '__react_static_root__/src/pages/404.js': require('__react_static_root__/src/pages/404.js').default,
+'__react_static_root__/src/pages/about.js': require('__react_static_root__/src/pages/about.js').default,
+'__react_static_root__/src/pages/blog.js': require('__react_static_root__/src/pages/blog.js').default,
+'__react_static_root__/src/pages/contact.js': require('__react_static_root__/src/pages/contact.js').default,
+'__react_static_root__/src/pages/faq.js': require('__react_static_root__/src/pages/faq.js').default,
+'__react_static_root__/src/pages/index.js': require('__react_static_root__/src/pages/index.js').default,
+'__react_static_root__/src/pages/online-coaching.js': require('__react_static_root__/src/pages/online-coaching.js').default,
+'__react_static_root__/src/pages/qualifications.js': require('__react_static_root__/src/pages/qualifications.js').default,
+'__react_static_root__/src/containers/Post': require('__react_static_root__/src/containers/Post').default
 }
-// Not Found Template
-export const notFoundTemplate = "__react_static_root__/src/pages/404.js"
+
+export const notFoundTemplate = '__react_static_root__/src/pages/404.js'
 
