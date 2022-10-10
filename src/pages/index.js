80ed74e4b4d9f6ce933bd311d84e2251d569bb0a
Marcel Hacker <marcelhacker@Marcels-MacBook-Air.local>
2022-10-10 15:18:16 +0200

    Add main post in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from "react";
-import Paper from "../components/Paper.js";
+import MainPost from "../components/MainPost.js";
 
 export default () => {
+  const mainFeaturedPost = {
+    title: "Title of a longer featured blog post",
+    description:
+      "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+    image: "https://source.unsplash.com/random",
+    imageText: "main image description",
+    linkText: "Continue reading…",
+  };
   return (
     <div>
-      <Paper />
+      <MainPost post={mainFeaturedPost} />
     </div>
   );
 };
