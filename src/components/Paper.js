30bde24ab6ff11c2a84d5ae9f3e0ba8ed784b6a1
Marcel Hacker <marcelhacker@Marcels-MacBook-Air.local>
2022-10-10 06:36:01 +0200

    Add test image in index.js

--- src/components/Paper.js
+++ src/components/Paper.js
@@ -2,15 +2,19 @@ import * as React from "react";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
-import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
+import coaching from "../../public/all-in-coaching.jpg";
+import { styled } from "@mui/material/styles";
 
 const Item = styled(Paper)(({ theme }) => ({
   textAlign: "center",
+  //"background-image": coaching,
   height: 60,
   lineHeight: "60px",
 }));
 
 const PaperImage = () => {
+  const height = "100em";
+  const width = "180em";
   return (
     <Grid container spacing={2}>
       <Grid item xs={6}>
@@ -21,12 +25,17 @@ const PaperImage = () => {
             display: "grid",
             gridTemplateColumns: { md: "1fr 1fr" },
             gap: 2,
-            width: "1500px",
-            height: "1000px",
+            width: width,
+            height: height,
           }}
         >
           <Item elevation={3}>
-            <img src={"../../public/all-in-coaching.jpg"} />
+            <img
+              src={coaching}
+              alt={"Coaching"}
+              width={width}
+              height={height}
+            />
           </Item>
         </Box>
       </Grid>
